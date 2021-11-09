e21cb5a91a1a4fecea1fbe4157045de53b1456ec
David Chu <dchu26@ucmerced.edu>
2021-11-08 22:11:57 -0800

    Completed Login Screen
    Initially opens app to login screen

--- Spots/Spots/SceneDelegate.swift
+++ Spots/Spots/SceneDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
@@ -17,6 +18,17 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
+        if PFUser.current() != nil {
+            // let main = UIStoryboard(name: "Main", bundle: nil)
+            
+            // initialize a different storyboard as the one to show
+            
+        }
+        else {
+            let storyboard = UIStoryboard(name: "LoginScreen", bundle: nil)
+            let viewController = storyboard.instantiateViewController(withIdentifier: "LoginViewController")
+            window?.rootViewController = viewController
+        }
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
