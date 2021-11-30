8710b0f8c6d24ebdb8a610809ec4ed1d17bdec84
eymi707 <amylau2000@gmail.com>
2021-11-30 03:29:19 -0800

    Places/Food List Screens

---
+++ Spots/PlacesListController.swift
@@ -0,0 +1,29 @@
+//
+//  PlacesListController.swift
+//  Spots
+//
+//  Created by Amy Lau on 11/30/21.
+//
+
+import UIKit
+
+class PlacesListController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
